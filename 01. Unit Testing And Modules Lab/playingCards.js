e52bf2a3b22cba6ceceb95faf95bc6d893a32ecf
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-10-26 14:52:05 +0200

    Added Playing Cards

--- 01. Unit Testing And Modules Lab/playingCards.js
+++ 01. Unit Testing And Modules Lab/playingCards.js
@@ -1,11 +1,35 @@
-function solution(first, second){
+function solution(face, suit){
     let faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
     let suits = {S: "\u2660", H: "\u2665", D: "\u2666", C: "\u2663"};
 
-        if (faces.includes(first) && suits.hasOwnProperty(second)) {
-            let suit = String.fromCharCode('\u2660');
-            console.log(first + suit);
+    class Card {
+        constructor(face, suit) {
+            this.face = face;
+            this.suit = suit;
         }
-    
+        get face() {
+            if (!faces.includes(this._face)) {
+                throw Error("Error");
+            }
+            return this._face;
+        }
+        set face(face) {
+            this._face = face;
+        }
+        get suit() {
+            if (!Object.keys(suits).includes(this._suit)) {
+                throw Error("Error");
+            }
+            return this._suit;
+        }
+        set suit(suit) {
+            this._suit = suit;
+        }
+        toString() {
+            return `${this.face}${suits[this.suit]}`;
+        }
+    }
+    let card = new Card(face, suit);
+    return card.toString();
 }
 solution('A', 'S')
