53b47fa8526eaa0f0386096cf97aa936af572919
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-10-27 21:32:40 +0200

    Refactoring

--- 02. REST Services And AJAX Exercises/01.BUS-STOP/app.js
+++ 02. REST Services And AJAX Exercises/01.BUS-STOP/app.js
@@ -1,19 +1,27 @@
 function getInfo() {
-    let baseURL = 'https://judgetests.firebaseio.com/businfo/{stopId}.json';
-
-    let stopID = document.querySelector("#stopId");
+    let stopID = document.querySelector("#stopId").value;
     let stationName = document.querySelector("#stopName");
     let busesEl = document.querySelector("#buses");
+    let url = `https://judgetests.firebaseio.com/businfo/${stopID}.json`;
+    let validIDs = ['1287', '1308', '1327', '2334'];
 
-    let validIDs = ["1287", "1308", "1327", "2334"];
-
-    if (!validIDs.includes(stopID.value)) {
+    if (validIDs.includes(stopID)) {
+        stationName.textContent = "";
+        clearList(busesEl);
+        fetch(url)
+        .then((response) => response.json())
+        .then((result) => showInfo(result));
+        
+    } else{
+        clearList(busesEl);
         stationName.textContent = "Error";
     }
-    let url = baseURL.replace("{stopId}", stopID.value);
-    fetch(url)
-    .then((response) => response.json())
-    .then((result) => showInfo(result));
+
+    function clearList(ul) {
+        while (ul.firstChild) {
+          ul.removeChild(ul.firstChild);
+        }
+      }
 
     function showInfo(data){
         stationName.textContent = data.name;
