a4e76a6917ae9d68fefa07bbbc6877179cb2c966
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-10-28 03:05:43 +0200

    Added Bus Schedule

--- 02. REST Services And AJAX Exercises/02.BUS-SCHEDULE/app.js
+++ 02. REST Services And AJAX Exercises/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,45 @@
 function solve() {
+    let info = document.getElementsByClassName("info")[0];
+    let departBtn = document.getElementById("depart");
+    let arriveBtn = document.getElementById("arrive");
+    let currentId = "depot";
 
     function depart() {
-        console.log('Depart TODO...');
+        let url = `https://judgetests.firebaseio.com/schedule/${currentId}.json `;
+
+        fetch(url)
+        .then((resource) => resource.json())
+        .then((result) => {info.textContent = `Next stop ${result.name}`})
+        .catch(Error());
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        let url = `https://judgetests.firebaseio.com/schedule/${currentId}.json `;
+        
+        fetch(url)
+        .then((resource) => resource.json())
+        .then((result) => {info.textContent = `Arriving at ${result.name}`
+        currentId = result.next})
+        .catch(Error());
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
         depart,
         arrive
     };
-}
 
+    function Error() {
+        return () => {
+            info.textContent = "Error";
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        };
+    }
+}
 let result = solve();
