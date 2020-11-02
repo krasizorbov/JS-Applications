6c7a522e5cf43a6946bbd158c46540a78a3459a3
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-11-02 15:05:49 +0200

    Refactoring

--- 05. Templating Exercises/01. List Towns/app.js
+++ 05. Templating Exercises/01. List Towns/app.js
@@ -1,18 +1,17 @@
 window.addEventListener("load", async () => {
     const template = await(await fetch('./main-template.hbs')).text();
+
+    // Partial Name = town; Calling partial is done by {{> town}} in main-template.hbs
     Handlebars.registerPartial('town', await(await fetch('./town-template.hbs')).text());
-    
+
     const templateFunc = Handlebars.compile(template);
 
     document.querySelector('#btnLoadTowns').addEventListener("click", showInfo);
 
-    const input = document.querySelector('#towns');
-    const rootEl = document.querySelector('#root');
-
     function showInfo(e){
         e.preventDefault();
-        const towns = input.value.split(', ');
+        const towns = document.querySelector('#towns').value.split(', ');
         const generateHTML = templateFunc({towns});
-        rootEl.innerHTML = generateHTML;
+        document.querySelector('#root').innerHTML = generateHTML;
     }
 });
