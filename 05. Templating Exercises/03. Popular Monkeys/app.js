b4c6d0ef986857fc015db613caa394ac9a60600d
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-11-03 13:18:20 +0200

    Added Monkeys

---
+++ 05. Templating Exercises/03. Popular Monkeys/app.js
@@ -0,0 +1,19 @@
+import {monkeys} from "./monkeys.js";
+window.addEventListener("load", async () => {
+    const section = document.querySelector("section");
+    const sectionString = await (await fetch('./main.hbs')).text();
+    const mainTemplate = Handlebars.compile(sectionString);
+    Handlebars.registerPartial('monkey', await (await fetch('./monkey.hbs')).text())
+    const html = mainTemplate({monkeys});
+    section.innerHTML = html;
+    const monkeysEl = document.querySelector('.monkeys');
+    monkeysEl.addEventListener("click", showData);
+
+    function showData(e) {
+        if (e.target.tagName !== "BUTTON") {
+            return;
+        }
+        const div = e.target.parentNode.querySelector('p');
+        div.removeAttribute('style');
+    }
+})
