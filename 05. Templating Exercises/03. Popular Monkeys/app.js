f06c88b38e3a069d4e38d567718545c28a5eccff
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-11-04 10:01:38 +0200

    Refactoring

--- 05. Templating Exercises/03. Popular Monkeys/app.js
+++ 05. Templating Exercises/03. Popular Monkeys/app.js
@@ -1,11 +1,14 @@
 import {monkeys} from "./monkeys.js";
 window.addEventListener("load", async () => {
     const section = document.querySelector("section");
+
     const sectionString = await (await fetch('./main.hbs')).text();
     const mainTemplate = Handlebars.compile(sectionString);
-    Handlebars.registerPartial('monkey', await (await fetch('./monkey.hbs')).text())
+    Handlebars.registerPartial('monkey', await (await fetch('./monkey.hbs')).text());
+
     const html = mainTemplate({monkeys});
     section.innerHTML = html;
+    
     const monkeysEl = document.querySelector('.monkeys');
     monkeysEl.addEventListener("click", showData);
 
