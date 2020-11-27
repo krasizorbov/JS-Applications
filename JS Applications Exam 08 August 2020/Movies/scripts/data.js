268011f79e7ddbe8c2bde407c1fc0520f5bb0ea0
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-11-27 06:43:33 +0200

    Refactoring

---
+++ JS Applications Exam 08 August 2020/Movies/scripts/data.js
@@ -0,0 +1,18 @@
+const apiID = "A2B80C1D-EF7D-9932-FF46-059793508A00";
+const apiKEY = "6CA1313E-5803-4ECC-8926-30CE898514BF";
+const registerURL = `https://api.backendless.com/${apiID}/${apiKEY}/users/register`;
+const loginURL = `https://api.backendless.com/${apiID}/${apiKEY}/users/login`;
+const logoutURL = `https://api.backendless.com/${apiID}/${apiKEY}/users/logout`;
+const shoesURL = `https://api.backendless.com/${apiID}/${apiKEY}/data/shoes`;
+const deleteURL = `https://api.backendless.com/${apiID}/${apiKEY}/data/shoes/`;
+const editShoesURL = `https://api.backendless.com/${apiID}/${apiKEY}/data/shoes/`;
+
+export async function register(email, password) {
+  const response = await fetch(registerURL, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await response.json();
+  return data;
+}
